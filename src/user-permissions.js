832e04e800e2fdeffe3a4e6654b3422982c1fb30
cody.wikman <cody.wikman@pgm-corp.com>
2018-07-17 15:00:14 -0700

    added basic tests for action and user context resolvers

--- src/user-permissions.js
+++ src/user-permissions.js
@@ -1,9 +1,61 @@
 class UserPermissions {
 
-    requires(action, context) {
-        return context.uprm.vote
+    constructor(actionProfile) {
+        const map = new Map()
+        for (let role in actionProfile) {
+            actionProfile[role].forEach(action => map.set(action, role))
+        }
+        this.profile = map
     }
 
+    /**
+     * 
+     * @param {*} action the requested action. May be a string or object containg many requirements
+     * @param {*} context the user text 
+     */
+    requires(action, context) {
+        return (typeof action === 'string') ? this._shallowResolve(action, context) : this._deepResolve(action, context)
+    }
+
+
+    // resolve simple actions by confirm the context has the action as a property
+    _shallowResolve(action, context) {
+        let requiredRole = this.profile.get(action)
+        return (context.uprm && context.uprm[requiredRole])
+    }
+
+    // performs deep resolve for properties and multi action
+    _deepResolve(action, context) {
+        // role checking
+        if (action.action) {
+            if (!this._shallowResolve(action.action, context)) {
+                return false
+            }
+        }
+        // checking each of the actions FAIL if any false
+        for (let act in action) {
+            // if not exists false
+            if (!context[act]) {
+                return false
+            }
+            if (!this._contextFoundInAction(action[act], context[act])) {
+                return false
+            }
+        }
+        return true
+    }
+
+    _contextFoundInAction(actionValue, contextValue) {
+        let av = [].concat(actionValue)
+        let cv = [].concat(contextValue)
+        let ans = cv.reduce((accum, v) => {
+            if (av.includes(v)){
+                accum = true
+            }
+            return accum
+        }, false)
+        return ans
+    }
 }
 
 module.exports = UserPermissions
--- src/user-permissions.test.js
+++ src/user-permissions.test.js
@@ -15,16 +15,16 @@ const actionProfile = {
 
 }
 
-const context = {
-    user: 32,
-    location: 'tokyo',
-    sub_locations: ['north', 'south', 'river'],
-    regions_codes: [362, 346, 123],
-    uprm: {
-        board_member: ['farm', 'fishing'],
-        citizen: true,
-    }
-}
+// const userContext = {
+//     user: 32,
+//     location: 'tokyo',
+//     sub_locations: ['north', 'south', 'river'],
+//     regionscodes: [362, 346, 123],
+//     uprm: {
+//         board_member: ['farm', 'fishing'],
+//         citizen: true,
+//     }
+// }
 
 
 var up;
@@ -33,12 +33,41 @@ beforeEach(() => {
 })
 
 
-test('verifies the user has the vote action', () => {
-    let context = {uprm:{vote: true}};
-    expect(up.requires('vote', context)).toBeTruthy()
+test('require finds single uprm returns true', () => {
+    // this context says that they are a user. Based on the action profile users are allowed to vote
+    let userContext = { uprm: { citizen: true } };
+    expect(up.requires('vote', userContext)).toBeTruthy()
 })
 
-test('verifies the user does not have vote', () => {
-    let context = {uprm:{}};
-    expect(up.requires('vote', context)).toBeFalsy()
+test('require does not find single uprm returns true', () => {
+    // This context is empty, so they con't have the vote action
+    let userContext = { uprm: {} };
+    expect(up.requires('vote', userContext)).toBeFalsy()
+})
+
+test('require returns true for multi matched context props', () => {
+    // this userContext has a propery of region code. Both 362 and 451
+    // Subsequently these action require the posession of each region code respectively
+    let action = { region_code: 362 }
+    let action2 = { region_code: 451 }
+    let userContext = { region_code: [362, 451] }
+    expect(up.requires(action, userContext)).toBeTruthy()
+    expect(up.requires(action2, userContext)).toBeTruthy()
+})
+
+test('require returns true for single context / multi action props', () => {
+    // this userContext has a propery of region code. Both 362 and 451
+    // Subsequently these action require the posession of each region code respectively
+    let action = { region_code: [362, 451] }
+    let userContext = { region_code: 451 }
+    expect(up.requires(action, userContext)).toBeTruthy()
+})
+
+
+test('require returns false for action prop value not found', () => {
+    // this userContext has a propery of region code. Both 362 and 451
+    // Subsequently these action require the posession of each region code respectively
+    let action = { region_code: [362, 451] }
+    let userContext = { region_code: 400 }
+    expect(up.requires(action, userContext)).toBeFalsy()
 })
